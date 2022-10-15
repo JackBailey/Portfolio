b21cbf7f028d3f9ec0f89d20cf146f83a686b0d4
Jack Bailey <jack@jackbailey.dev>
2022-10-15 17:06:18 +0100

    Switch back to google fonts

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
 <script></script>
 
 <style lang="scss">
-@import url("https://fonts.bunny.net/css2?family=Montserrat:wght@200;300;400;500;600;700&display=swap");
+@import url("https://fonts.googleapis.com/css2?family=Montserrat:wght@200;300;400;500;600;700&display=swap");
 #app {
 	min-height: 100%;
 	font-family: "Montserrat", sans-serif;
