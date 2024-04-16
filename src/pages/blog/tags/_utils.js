afee85fae65d5bf7023b78ef1a8efb3504426e56
Jack Bailey <81699395+JackBailey@users.noreply.github.com>
2024-04-16 20:52:25 +0100

    Add tag support

---
+++ src/pages/blog/tags/_utils.js
@@ -0,0 +1,24 @@
+import { getCollection } from 'astro:content';
+
+export const getTagMap = async () => {
+    let blogEntries = await getCollection('blog');
+
+    let allTags = [];
+    blogEntries.forEach((post) => {
+        if (post.data.tags) {
+            const tags = post.data.tags.split(",");
+            allTags = allTags.concat(tags);
+        }
+    });
+
+    const uniqueTags = [...new Set(allTags)];
+
+    const tagMap = new Map();
+    uniqueTags.forEach((tag) => {
+        const lowercaseTagId = tag.toLowerCase().replace(/ /g, "_");
+        const tagWithSpaces = tag.replace(/_/g, " ");
+        tagMap.set(lowercaseTagId, tagWithSpaces);
+    });
+
+    return tagMap;
+};
