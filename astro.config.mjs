15430c99a237bb64c93dabedf447aa34a054311c
Jack Bailey <81699395+JackBailey@users.noreply.github.com>
2024-04-19 19:26:50 +0100

    Add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,20 @@
 import { defineConfig } from 'astro/config';
 import markdoc from "@astrojs/markdoc";
-
 import vercel from "@astrojs/vercel/serverless";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://jackbailey.dev",
   markdown: {
     shikiConfig: {
       theme: "andromeeda"
     }
   },
-  integrations: [markdoc()],
+  integrations: [markdoc(), sitemap()],
   output: "hybrid",
   adapter: vercel({
-    edgeMiddleware: true,
+    edgeMiddleware: true
   })
 });
