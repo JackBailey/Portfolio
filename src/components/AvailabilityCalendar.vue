59b554bbe042eb7b641c27de5538c9725638e0e7
Jack Bailey <81699395+JackBailey@users.noreply.github.com>
2025-09-10 09:39:59 +0100

    Improve calendar

--- src/components/AvailabilityCalendar.vue
+++ src/components/AvailabilityCalendar.vue
@@ -1,70 +1,105 @@
 <template>
     <div class="calendar-outer">
+        <div class="loading" v-if="loading">
+            <span class="loader"></span>
+            <span>Loading...</span>
+        </div>
         <FullCalendar :options="calendarOptions" />
+        
     </div>
 </template>
 
 <script>
 import FullCalendar from "@fullcalendar/vue3";
 import dayGridPlugin from "@fullcalendar/daygrid";
-import ICAL from "ical.js";
 
 export default {
     components: {
         FullCalendar,
     },
     data() {
+        const now = new Date();
+        const firstOfMonth = new Date(now.getFullYear(), now.getMonth(), 1);
+        const firstOfMonthStr = [
+            firstOfMonth.getFullYear(),
+            String(firstOfMonth.getMonth() + 1).padStart(2, '0'),
+            String(firstOfMonth.getDate()).padStart(2, '0')
+        ].join('-');
         return {
             calendarOptions: {
                 plugins: [dayGridPlugin],
                 height: "auto",
                 firstDay: 1,
                 defaultAllDay: true,
+                validRange: {
+                    start: firstOfMonthStr,
+                },
             },
+            loading: true,
         };
     },
     methods: {
-        // Inspired by https://github.com/larrybolt/online-ics-feed-viewer/blob/33ebdae5c44048517507b82105b1230d7319ba3b/script.js#L16
         load_ics(ics_data) {
-            const parsed = ICAL.parse(ics_data);
-            const events = [];
-            parsed[2].map(([type, event_fields]) => {
-                if (type !== "vevent") {
-                    return;
-                }
-                
-                const start = new Date(event_fields.find((item) => item[0] === "dtstart")[3]);
-                const end = event_fields.find((item) => item[0] === "dtend")?.[3] ? new Date(event_fields.find((item) => item[0] === "dtend")?.[3]) : null;
-                const title = event_fields.find((item) => item[0] === "summary")[3];
-                if (start && end) {
-                    const totalDays = Math.ceil((end - start) / (1000 * 60 * 60 * 24));
-                    if (totalDays > 1) {
-                        for (let i = 0; i < totalDays; i++) {
-                            const day = new Date(start);
-                            day.setDate(start.getDate() + i);
-                            events.push({ start: day, end: day, title: i === 0 ? title : "", display: "background" });
-                        }
-                        return;
-                    }
-                }
-
-
-                events.push({ start, end, title, display: "background" });
-            });
-
-            this.calendarOptions.events = events;
+            this.calendarOptions.events = ics_data;
         },
     },
     async mounted() {
+        this.loading = true;
+        await new Promise((r) => setTimeout(r, 1500)); // Simulate loading time
         const response = await fetch("/api/calendar");
-        const icalData = await response.text();
+        const icalData = await response.json();
         this.load_ics(icalData);
+        this.loading = false;
     },
 };
 </script>
 
 <style lang="scss" scoped>
 .calendar-outer {
+    position: relative;
+    .loading {
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        background-color: var(--background-1);
+        padding: 1rem 2rem;
+        border-radius: 8px;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        gap: 1rem;
+        box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+        transform: translate(-50%, -50%);
+        z-index: 5;
+        .loader {
+            width: 48px;
+            height: 48px;
+            display: inline-block;
+            position: relative;
+            border: 3px solid;
+            border-color: var(--accent) #0000 var(--foreground-1) #0000;
+            border-radius: 50%;
+            box-sizing: border-box;
+            animation: 1s rotate linear infinite;
+        }
+        .loader:before , .loader:after{
+            content: '';
+            top: 0;
+            left: 0;
+            position: absolute;
+            border: 10px solid transparent;
+            border-bottom-color: var(--foreground-1);
+            transform: translate(-10px, 19px) rotate(-35deg);
+        }
+        .loader:after {
+            border-color: var(--accent) #0000 #0000 #0000 ;
+            transform: translate(32px, 3px) rotate(-35deg);
+        }
+        @keyframes rotate {
+            100%{    transform: rotate(360deg)}
+        }
+
+    }
     @media screen and (max-width: 768px) {
         :deep(.fc) {
             .fc-header-toolbar.fc-toolbar {
--- src/pages/api/calendar.js
+++ src/pages/api/calendar.js
@@ -1,4 +1,5 @@
 import dotenv from "dotenv";
+import ICAL from "ical.js";
 
 dotenv.config();
 
@@ -7,9 +8,10 @@ export const prerender = false;
 const icalURL = process.env.ICAL_LINK;
 
 const cache = {
-    data: null,
+    events: [],
     lastUpdated: null,
-    ttl: 1000 * 60 * 5, // 5 min
+    ttl: 1000 * 60 * 5, // 5 min,
+    contentType: "application/json",
 };
 
 const cacheHeaders = {
@@ -26,9 +28,9 @@ export const GET = async () => {
         });
     }
 
-    if (cache.data && cache.lastUpdated + cache.ttl > Date.now()) {
+    if (cache.events && cache.lastUpdated + cache.ttl > Date.now()) {
         console.log("Using cache");
-        return new Response(cache.data, {
+        return new Response(JSON.stringify({events: cache.events}), {
             status: 200,
             headers: cacheHeaders,
         });
@@ -36,23 +38,58 @@ export const GET = async () => {
 
     try {
         const response = await fetch(icalURL);
-        const data = await response.text();
+        const icalData = await response.text();
 
-        cache.data = data;
+        const parsed = ICAL.parse(icalData);
+        const events = [];
+        // Inspired by https://github.com/larrybolt/online-ics-feed-viewer/blob/33ebdae5c44048517507b82105b1230d7319ba3b/script.js#L16
+        const firstOfMonth = new Date();
+        firstOfMonth.setDate(0); // Set to last day of previous month
+        firstOfMonth.setHours(0, 0, 0, 0);
+        parsed[2].map(([type, event_fields]) => {
+            if (type !== "vevent") {
+                return;
+            }
+            
+            const start = new Date(event_fields.find((item) => item[0] === "dtstart")[3]);
+            const end = event_fields.find((item) => item[0] === "dtend")?.[3] ? new Date(event_fields.find((item) => item[0] === "dtend")?.[3]) : null;
+
+            // If the event is before the start of the current month, skip it
+            if (end && end < firstOfMonth) {
+                return;
+            }
+
+            if (start && end) {
+                const totalDays = Math.ceil((end - start) / (1000 * 60 * 60 * 24));
+                if (totalDays > 1) {
+                    for (let i = 0; i < totalDays; i++) {
+                        const day = new Date(start);
+                        day.setDate(start.getDate() + i);
+                        events.push({ start: day, end: day, title: i === 0 ? "Busy" : "", display: "background" });
+                    }
+                    return;
+                }
+            }
+
+
+            events.push({ start, end, title: "Busy", display: "background" });
+        });
+
+        cache.events = events;
         cache.lastUpdated = Date.now();
 
         console.log("Updated cache");
 
-        return new Response(data, {
+        return new Response(JSON.stringify({events}), {
             status: 200,
             headers: cacheHeaders,
         });
     } catch (error) {
-        return new Response(null, {
+        return new Response(JSON.stringify({
+            error: "Failed to fetch calendar"
+        }), {
             status: 500,
             statusText: "Internal Server Error",
         });
     }
-
-    return redirect(`/contact/success?id=${submission.id}`);
 };
